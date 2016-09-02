fdf66782741122d304df82d7fe58de89d5f9281e
cyqresig <moonsunfall@aliyun.com>
2016-09-03 00:45:12 +0800

    changed the type of export

--- easing.js
+++ easing.js
@@ -1,10 +1,11 @@
 
-const easing = {
-    easeOutCirc (x, t, b, c, d) {
-        return c * Math.sqrt(1 - (t = t/d - 1) * t) + b;
-    }
+
+export const easeOutCirc = (x, t, b, c, d) => {
+    return c * Math.sqrt(1 - (t = t/d - 1) * t) + b;
 }
 
-export default easing
+export default {
+    easeOutCirc,
+}
 
 
